e42257a60368751f3b511c3cd3a95c0bbdabd9a4
Zennuba <loni70540@edu.ucl.dk>
2025-09-04 10:26:26 +0200

    update

--- Lottes filer/script_newsletterForm_footer.js
+++ Lottes filer/script_newsletterForm_footer.js
@@ -1,7 +1,11 @@
 <-- Lotte newsletter>
 
 const newsletterform = document.getElementById("newsletter-form");
-if(newletter-email) {
+
+
+
+/* AI-prompt ChatGPT:
+jeg skal lave js til en newsletter form. Opgaven er at lave Javascript (simpelt) Hjælp mig, uden at give mig koden, stil mig spørgsmål indtil jeg har den selv:(html kode indsat)*/
  
 
 
