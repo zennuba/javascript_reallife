cccc7a7558c350c33357e095887fe6d3fa61c657
Zennuba <loni70540@edu.ucl.dk>
2025-09-05 09:43:42 +0200

    footer-nav.js update final

--- footer-nav.js
+++ footer-nav.js
@@ -7,51 +7,44 @@
     .then(response => response.json())
       .then(data => {
         console.log(data);
-      }
+      
 
-  const footerNav = document.getElementById("footer-nav"); 
+    const footerNav = document.getElementById("footer-nav"); 
+   
+    /*Loop over JSON-sektionerne */
+      for (const section in data) 
+        {
+          const column = document.createElement("div");
+          column.classList.add("footer-nav__column");
 
+          const title = document.createElement("h3");
+          title.classList.add("footer-nav__title");
+          title.textContent = section;
+          column.appendChild(title);
 
-    /*
- // Loop over JSON-sektionerne */
-    for (const section in data) {
-      const column = document.createElement("div");
-      column.classList.add("footer-nav__column");
+          const ul = document.createElement("ul");
+            ul.classList.add("footer-nav__list");
 
-        const title = document.createElement("h3");
-        title.classList.add("footer-nav__title");
+          /* loop for hvert item oprettes en li + en a */
 
-        title.textContent = section;
-        column.appendChild(title);
+      data[section].forEach(item => 
+        {
+          const li = document.createElement("li");
+            li.classList.add("footer-nav__item"); 
 
-      const ul = document.createElement("ul");
-      ul.classList.add("footer-nav__list");
-
-    /* for hver sektion skal der oprettes en li + en a */
-
-      data[section].forEach(item => {
-        const li = document.createElement("li");
-        li.classList.add("footer-nav__item"); 
-
-      })
-
-        const a = document.createElement("a"); 
-          a.textContent = item.title;  // teksten
-          a.href = item.url;            // linket
+          const a = document.createElement("a"); 
+            a.textContent = item.title;  // teksten fra JSON
+            a.href = item.url;            // linket fra JSON
 
 
-            li.appendChild(a); //tilføjer child til li
-            ul.appendChild(li); //tilføjer child til ul
-          }); 
-          //parantes fra "then (data => "
+              li.appendChild(a);  //tilføjer child til li
+              ul.appendChild(li); //tilføjer child til ul
+        }); 
 
       column.appendChild(ul);
        footerNavNav.appendChild(column);
-
-  
-            let butikker = data.Butikker;
-            console.log(butikker); // ["København Comics & Potter", "København Bræt- & Rollespil"]
-        
-        
-        .catch(error => console.error("Fejl ved indlæsning:", error));
-
+      }
+      }) 
+            
+        .catch(error => console.error("Fejl ved indlæsning:", error)
+    )
